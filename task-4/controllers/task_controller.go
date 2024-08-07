ffd980abfc1dcfab2a8ae41b18ce3313fb3f5d41
anatoli-derese <anatoli.derese@aait.edu.et>
2024-08-07 14:11:54 +0300

    task 4 finished

--- task-4/controllers/task_controller.go
+++ task-4/controllers/task_controller.go
@@ -1,17 +1,13 @@
 package controllers
 
 import (
-	"backend-learning-track/task-4/data"
+	"backend-learning-track/task-4/router"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Runner(router *gin.Engine) {
-	router.GET("/tasks", data.GetAllTask)
-	router.GET("/tasks/:id", data.GetSpecificTask)
-	router.POST("/tasks", data.AddNewTask)
-	router.DELETE("/tasks/:id", data.DeleteTask)
-	router.PUT("/tasks/:id", data.UpdateTask)
-	router.Run("localhost:8080")
+func Runner(r *gin.Engine) {
+	router.SetUpRouter(r)
+	r.Run("localhost:8080")
 
 }
--- task-4/router/router.go
+++ task-4/router/router.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"backend-learning-track/task-4/data"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetUpRouter(router *gin.Engine) {
+	router.GET("/tasks", data.GetAllTask)
+	router.GET("/tasks/:id", data.GetSpecificTask)
+	router.POST("/tasks", data.AddNewTask)
+	router.PUT("/tasks/:id", data.UpdateTask)
+	router.DELETE("/tasks/:id", data.DeleteTask)
+
+}
